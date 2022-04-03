0c29f0aa25aa10de700a2fe6deb346faed56a09a
hwachoo <101535037+hwachoo@users.noreply.github.com>
2022-04-03 20:40:50 +0900

    Add files via upload

---
+++ Project/src/ProjectSasmple/QuizExe.java
@@ -0,0 +1,9 @@
+package ProjectSasmple;
+
+public class QuizExe {
+	public static void main(String[] args) {
+		QuizApp app = new QuizApp();
+		app.execute();
+	}
+
+}
---
+++ Project/src/ProjectSasmple/QuizService.java
@@ -0,0 +1,13 @@
+package ProjectSasmple;
+
+import java.util.List;
+
+public interface QuizService {
+	
+	public void insertQuiz(Quiz quiz); //문제등록
+	public List<Quiz> getQuiz(); //문제 풀기
+	public List<Quiz> Quizlist(); //문제리스트
+	public void modifyQuiz(Quiz quiz); //문제 수정
+	public void deleteQuiz(int qn); //문제 삭제
+
+}
